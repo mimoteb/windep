ed0e3d8e274db1c8f83a136b1296a63a96f20e1d
Solomon Samuel <sissona@live.fr>
2024-01-31 09:13:06 +0100

    corrections

--- online/automate.ps1
+++ online/automate.ps1
@@ -1,10 +1,23 @@
 $repo = 'https://github.com/mimoteb/windep/archive/refs/heads/main.zip'
 $repo_OutFile = "$env:TEMP\windep-main.zip"
-write-host 'Downloading from' $repo
-write-host 'downloading to' $repo_OutFile
-Invoke-WebRequest -Uri $repo -OutFile $repo_OutFile
-# Extract the zip file
-write-host 'Extracting the repo'
-Expand-Archive -Path $repo_OutFile -DestinationPath "$env:TEMP"
-write-host 'Chaninng the current directory to \windep-main\'
-set-location -Path "$env:TEMP\windep-main\"
+$extractedPath = "$env:TEMP"
+
+try {
+    Write-Host "Downloading from $repo"
+    Write-Host "Downloading to $repo_OutFile"
+    Invoke-WebRequest -Uri $repo -OutFile $repo_OutFile -ErrorAction Stop
+
+    Write-Host "Extracting the repo"
+    Expand-Archive -Path $repo_OutFile -DestinationPath $extractedPath -ErrorAction Stop
+
+    Write-Host "Changing the current directory to $extractedPath"
+    Set-Location -Path $extractedPath
+}
+catch {
+    Write-Host "An error occurred: $_" -ForegroundColor Red
+    # Handle the error as needed (log, cleanup, etc.)
+}
+finally {
+    # Cleanup: Remove the downloaded zip file
+    Remove-Item -Path $repo_OutFile -ErrorAction SilentlyContinue
+}
