1f913b5f4ea0254c047c60a8fd3120dc7f1dfd23
Solomon Samuel <sissona@live.fr>
2024-01-31 09:54:40 +0100

    disable services

--- online/disable_services.ps1
+++ online/disable_services.ps1
@@ -1,1 +1,20 @@
-write-host $_.
+write-host 'Disabling services/ stopping them temporarily'
+$Services = Get-Content -Path .\lists\services.lst
+
+foreach ($Service in $Services)
+{
+    write-host $Service
+    # try 
+    # {
+    #     Stop-Service -Name $Service -ErrorAction SilentlyContinue
+	#     net stop $Service
+	# }
+    
+    # catch{   }
+    # try
+    # {
+    #     Set-Service -Name $Service -StartupType 'Disabled' -ErrorAction SilentlyContinue
+    #     sc config $Service start=disabled
+    # }
+    # catch    {  }
+}
