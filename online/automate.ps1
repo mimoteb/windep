161cb429ec8c68a8fc0328270dd46ab9f704d32e
Solomon Samuel <sissona@live.fr>
2024-01-31 08:30:03 +0100

    correct the env var

--- online/automate.ps1
+++ online/automate.ps1
@@ -1,5 +1,5 @@
 $repo = 'https://github.com/mimoteb/windep/archive/refs/heads/main.zip'
-$repo_OutFile = '%temp%\windep.zip'
+$repo_OutFile = '$env:TEMP\windep.zip'
 Invoke-WebRequest -Uri $repo -OutFile $repo_OutFile
 # Extract the zip file
 Expand-Archive -Path "$env:TEMP\windep.zip" -DestinationPath "$env:TEMP"
