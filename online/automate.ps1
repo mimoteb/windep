2ee3fa0a92f0781ec4aedb24efc6b02fd39e1bae
Solomon Samuel <sissona@live.fr>
2024-01-31 08:24:44 +0100

    adding script to automate

--- online/automate.ps1
+++ online/automate.ps1
@@ -1,2 +1,5 @@
+$repo = 'https://raw.githubusercontent.com/mimoteb/win/main/online/automate.ps1?'
+$repo_OutFile = '%temp%\windep.zip'
+Invoke-WebRequest -Uri $repo -OutFile $repo_OutFile
 # Extract the zip file
-Expand-Archive -Path "$env:TEMP\main.zip" -DestinationPath "$env:TEMP"
+Expand-Archive -Path "$env:TEMP\windep.zip" -DestinationPath "$env:TEMP"
