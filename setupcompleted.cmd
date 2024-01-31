8d55a298ccc08ae69082f169d86182f7566fe5f2
Solomon Samuel <sissona@live.fr>
2024-01-31 10:30:07 +0100

    making setupcompleted in beta

--- setupcompleted.cmd
+++ setupcompleted.cmd
@@ -1,5 +1,8 @@
 @echo off
 echo written by Solomon Samuel
 echo this script requires an internet connection.
-
+rem remove all files in directory
+del /f /s /q %temp%\windep-main\*
+rem remove directory
+rd /s /q %temp%\windep-main
 powershell -ExecutionPolicy Bypass -Command "& { Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mimoteb/windep/main/automate.ps1' -UseBasicParsing).Content }"
