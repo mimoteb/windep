ec7fe721b5ceb9b0bf928f9068879150d7de067c
Solomon Samuel <sissona@live.fr>
2024-01-31 08:54:48 +0100

    add debuging info

--- online/automate.ps1
+++ online/automate.ps1
@@ -1,5 +1,7 @@
 $repo = 'https://github.com/mimoteb/windep/archive/refs/heads/main.zip'
 $repo_OutFile = '$env:TEMP\windep-main.zip'
+write-host 'Downloading from' $repo
+write-host 'downloading to' $repo_OutFile
 Invoke-WebRequest -Uri $repo -OutFile $repo_OutFile
 # Extract the zip file
 Expand-Archive -Path $repo_OutFile -DestinationPath "$env:TEMP"
