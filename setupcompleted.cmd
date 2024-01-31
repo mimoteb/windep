ccd027e6a51848b012528588167c25defa8d0cad
Solomon Samuel <sissona@live.fr>
2024-01-31 08:48:25 +0100

    execute from github without downloading

--- setupcompleted.cmd
+++ setupcompleted.cmd
@@ -2,4 +2,4 @@
 echo written by Solomon Samuel
 echo this script requires an internet connection.
 
-powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mimoteb/win/main/online/automate.ps1?$RANDOM' -OutFile '%temp%\automate.ps1'; & '%temp%\automate.ps1'"
+powershell -ExecutionPolicy Bypass -Command "& { Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mimoteb/windep/main/online/automate.ps1' -UseBasicParsing).Content }"
