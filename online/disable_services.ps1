c8cffa1d7ea93113bcc504208f4f6e4bbb6aabd7
Solomon Samuel <sissona@live.fr>
2024-03-02 14:32:32 +0100

    update

--- online/disable_services.ps1
+++ online/disable_services.ps1
@@ -5,7 +5,7 @@ foreach ($item in $items)
 {
     try 
     {
-        write-host 'Stopping' $item
+        write-host 'Stopping' $item -ForegroundColor Yellow
         Stop-Service -Name $item -ErrorAction SilentlyContinue
 	    net stop $item
 	}
@@ -13,7 +13,7 @@ foreach ($item in $items)
     catch{   }
     try
     {
-        write-host 'Disabling' $item
+        write-host 'Disabling' $item -ForegroundColor Yellow
         Set-Service -Name $item -StartupType 'Disabled' -ErrorAction SilentlyContinue
         sc config $item start=disabled
     }
