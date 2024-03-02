239085de395b619b6a6c440d7cb1c50039a62d6c
Solomon Samuel <sissona@live.fr>
2024-03-02 22:50:55 +0100

    update registry files

--- tmp/registry/ps1/disable_cortana.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host 'this script will disable cortana'
-if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search") -ne $true)
-{New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force -ErrorAction Continue};
-
-New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search' -Name 'AllowCortana' -Value 0 -PropertyType DWord -Force -ErrorAction Continue
