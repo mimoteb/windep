71e753c0637d1839a793447d8a7b833fae10c016
Solomon Samuel <sissona@live.fr>
2024-03-02 18:08:21 +0100

    updated

--- online/debloat.ps1
+++ online/debloat.ps1
@@ -7,24 +7,22 @@ $items = Get-Content -Path $tempPath
 foreach ($item in $items) {
     $item = $item.Trim()
 
-    try {
-        Get-AppxPackage -all *$item* | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
-        Get-AppxPackage -all *$item* | Remove-AppPackage -ErrorAction Continue
-        # WRONG: Remove-AppxPackage -Package $item -AllUsers -ErrorAction SilentlyContinue
-        Write-Host "[Success] AppxPackag removed: $item" -ForegroundColor Green
-    } catch {
-        Write-Host "[Error]: Failed to remove $item - $_" -ForegroundColor Red
-    }
-
     # try {
-    #     Write-Host "[AppxProvisionedPackage] Removing: $item" -ForegroundColor Cyan
-    #     #Remove-AppxProvisionedPackage -package $item -Online -ErrorAction SilentlyContinue
-    #     Write-Host "Successfully removed provisioned package: $item" -ForegroundColor Green
+    #     Get-AppxPackage -all *$item* | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
+    #     Get-AppxPackage -all *$item* | Remove-AppPackage -ErrorAction Continue
+    #     # WRONG: Remove-AppxPackage -Package $item -AllUsers -ErrorAction SilentlyContinue
+    #     Write-Host "[Success] AppxPackag removed: $item" -ForegroundColor Green
     # } catch {
     #     Write-Host "[Error]: Failed to remove $item - $_" -ForegroundColor Red
     # }
-}
 
+    try {
+        #Remove-AppxProvisionedPackage -package $item -Online -ErrorAction SilentlyContinue
+        Write-Host "[Success] removed provisioned package: $item" -ForegroundColor Green
+    } catch {
+        Write-Host "[Error]: Failed to remove $item - $_" -ForegroundColor Red
+    }
+}
 
 # Write-Host '[Info] removing windows capabilities' -ForegroundColor Yellow
 # $tempPath = [System.IO.Path]::Combine($env:TEMP, 'windep-main\lists\capabilities.lst')
