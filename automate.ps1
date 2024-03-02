7a20cb73b75b3e92a1eccd0715f9e280bd1b57cb
Solomon Samuel <sissona@live.fr>
2024-03-02 14:51:57 +0100

    coloring the output

--- automate.ps1
+++ automate.ps1
@@ -5,14 +5,14 @@ $repo_OutFile = "$env:TEMP\windep-main.zip"
 $extractedPath = "$env:TEMP"
 
 try {
-    Write-Host "Downloading from $repo"
-    Write-Host "Downloading to $repo_OutFile"
+    Write-Host "Downloading from $repo" -ForegroundColor Cyan
+    Write-Host "Downloading to $repo_OutFile" -ForegroundColor Yellow
     Invoke-WebRequest -Uri $repo -OutFile $repo_OutFile -ErrorAction Stop
 
-    Write-Host "Extracting the repo"
+    Write-Host "Extracting the repo" -foregroundcolor cyan
     Expand-Archive -Path $repo_OutFile -DestinationPath $extractedPath -ErrorAction Stop
 
-    Write-Host "Changing the current directory to $extractedPath"
+    Write-Host "[Info] Changing the current directory to $extractedPath" -ForegroundColor Yellow
     
 }
 catch {
@@ -29,7 +29,7 @@ Write-Host "Current Directory: $($PWD.Path)"
 # this will be the main script
 $scripts = Get-ChildItem -path "$env:TEMP\windep-main\online\*.ps1"  | Sort-Object
 foreach ($script in $scripts) {
-    write-host 'Executing:' $script
+    write-host '[Info] Executing:' $script -ForegroundColor cyan
     Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -File $script" -Wait
 }
 
