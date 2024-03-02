de918938945ecd28e32909006ff50333ebdb7e21
Solomon Samuel <sissona@live.fr>
2024-03-02 18:06:06 +0100

    updated log

--- online/debloat.ps1
+++ online/debloat.ps1
@@ -8,11 +8,10 @@ foreach ($item in $items) {
     $item = $item.Trim()
 
     try {
-        Write-Host "[AppxPackag] Removing: $item" -ForegroundColor Cyan
         Get-AppxPackage -all *$item* | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
 
         # WRONG: Remove-AppxPackage -Package $item -AllUsers -ErrorAction SilentlyContinue
-        Write-Host "Successfully removed package: $item" -ForegroundColor Green
+        Write-Host "[Success] AppxPackag removed: $item" -ForegroundColor Green
     } catch {
         Write-Host "[Error]: Failed to remove $item - $_" -ForegroundColor Red
     }
