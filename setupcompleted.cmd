0e95f941a61662dede64dd643c1ea18d377d1a29
Solomon Samuel <sissona@live.fr>
2024-03-02 11:51:44 +0100

    update

--- setupcompleted.cmd
+++ setupcompleted.cmd
@@ -6,10 +6,4 @@ del /f /s /q %temp%\windep-main\*
 rem remove directory
 rd /s /q %temp%\windep-main
 powershell -ExecutionPolicy Bypass -Command "& { Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mimoteb/windep/main/automate.ps1' -UseBasicParsing).Content }"
-
-
-@echo off
-echo written by Solomon Samuel
-echo this script requires an internet connection.
-
-powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mimoteb/win/main/online/automate.ps1?' -OutFile '%temp%\automate.ps1'; & '%temp%\automate.ps1'"
+powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mimoteb/windep/main/automate.ps1?' -OutFile '%temp%\automate.ps1'; & '%temp%\automate.ps1'"
