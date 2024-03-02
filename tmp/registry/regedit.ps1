a5dbd6d74ef8c25578ea51b40f40ba74cac129c3
Solomon Samuel <sissona@live.fr>
2024-03-02 22:39:03 +0100

    working on reg

--- tmp/registry/regedit.ps1
+++
@@ -1,8 +0,0 @@
-$regList = Get-ChildItem -Path .\registry -Filter "*.reg"
-foreach ($reg in $regList)
-{
-    regedit /s $reg
-}
-
-Get-ChildItem -Path .\ps1 -Filter "*.ps1" | ForEach-Object { & $_.FullName }
-
