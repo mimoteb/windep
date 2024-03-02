6d06de687ad527439954b02dd138204dd8882dc9
Solomon Samuel <sissona@live.fr>
2024-03-02 11:55:04 +0100

    update

--- setupcompleted.cmd
+++ setupcompleted.cmd
@@ -3,8 +3,7 @@ echo written by Solomon Samuel
 echo this script requires an internet connection.
 echo This script should be inside a windows image in the location:
 echo Mount_Dir\Windows\Setup\Scripts\
-rem remove all files in directory
+echo cleaning update %temp%\windep-main\
 del /f /s /q %temp%\windep-main\*
-rem remove directory
 rd /s /q %temp%\windep-main
 powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mimoteb/windep/main/automate.ps1?' -OutFile '%temp%\automate.ps1'; & '%temp%\automate.ps1'"
