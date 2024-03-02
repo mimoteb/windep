7b507db01fd22af5305c0d44a0e241b6a0f1028e
Solomon Samuel <sissona@live.fr>
2024-03-02 14:55:27 +0100

    more colors

--- automate.ps1
+++ automate.ps1
@@ -5,14 +5,14 @@ $repo_OutFile = "$env:TEMP\windep-main.zip"
 $extractedPath = "$env:TEMP"
 
 try {
-    Write-Host "Downloading from $repo" -ForegroundColor Cyan
-    Write-Host "Downloading to $repo_OutFile" -ForegroundColor Yellow
+    Write-Host "[Downloading] from $repo" -ForegroundColor Cyan
+    Write-Host "[Info] to $repo_OutFile" -ForegroundColor Yellow
     Invoke-WebRequest -Uri $repo -OutFile $repo_OutFile -ErrorAction Stop
 
-    Write-Host "Extracting the repo" -foregroundcolor cyan
+    Write-Host "[Extracting] the repo" -foregroundcolor cyan
     Expand-Archive -Path $repo_OutFile -DestinationPath $extractedPath -ErrorAction Stop
 
-    Write-Host "[Info] Changing the current directory to $extractedPath" -ForegroundColor Yellow
+    Write-Host "[Changing] the current directory to $extractedPath" -ForegroundColor Yellow
     
 }
 catch {
@@ -24,12 +24,12 @@ finally {
     Remove-Item -Path $repo_OutFile -ErrorAction SilentlyContinue
 }
 set-location -Path "$env:TEMP\windep-main"
-Write-Host "Current Directory: $($PWD.Path)"
+Write-Host "Current Directory: $($PWD.Path)" -foregroundcolor yellow
 
 # this will be the main script
 $scripts = Get-ChildItem -path "$env:TEMP\windep-main\online\*.ps1"  | Sort-Object
 foreach ($script in $scripts) {
-    write-host '[Info] Executing:' $script -ForegroundColor cyan
+    write-host '[Executing] :' $script -ForegroundColor cyan
     Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -File $script" -Wait
 }
 
