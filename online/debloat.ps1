370d6bbf6644e2475c814a64f9d4c3acf8b7e8fa
Solomon Samuel <sissona@live.fr>
2024-03-02 18:23:32 +0100

    mistakes corrected

--- online/debloat.ps1
+++ online/debloat.ps1
@@ -7,19 +7,17 @@ $items = Get-Content -Path $tempPath
 foreach ($item in $items) {
     $item = $item.Trim()
 
-    # try {
-    #     Get-AppxPackage -all *$item* | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
-    #     Get-AppxPackage -all *$item* | Remove-AppPackage -ErrorAction Continue
-    #     # WRONG: Remove-AppxPackage -Package $item -AllUsers -ErrorAction SilentlyContinue
-    #     Write-Host "[Success] AppxPackag removed: $item" -ForegroundColor Green
-    # } catch {
-    #     Write-Host "[Error]: Failed to remove $item - $_" -ForegroundColor Red
-    # }
+    try {
+        Get-AppxPackage -all *$item* | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
+        Get-AppxPackage -all *$item* | Remove-AppPackage -ErrorAction Continue
+        # WRONG: Remove-AppxPackage -Package $item -AllUsers -ErrorAction SilentlyContinue
+        Write-Host "[Success] AppxPackag removed: $item" -ForegroundColor Green
+    } catch {
+        Write-Host "[Error]: Failed to remove $item - $_" -ForegroundColor Red
+    }
 
     try {
         Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -match [regex]::Escape($item) } | Remove-AppxProvisionedPackage -AllUsers -ErrorAction SilentlyContinue
-
-        #Remove-AppxProvisionedPackage -package $item -Online -ErrorAction SilentlyContinue
         Write-Host "[Success] removed provisioned package: $item" -ForegroundColor Green
     } catch {
         Write-Host "[Error]: Failed to remove $item - $_" -ForegroundColor Red
