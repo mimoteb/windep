28e0de90e7358b6b1365130dd6e04890c8b6239d
Solomon Samuel <sissona@live.fr>
2024-03-02 14:27:49 +0100

    removing uncessary files

--- tmp/cleanup.cmd
+++ tmp/cleanup.cmd
@@ -1,4 +1,8 @@
-
+echo removing junk files
+DEL /F /S /Q /A %LocalAppData%\Microsoft\Windows\Explorer\thumbcache_*.db
+del /F /Q %APPDATA%\Microsoft\Windows\Recent\*
+del /F /Q %APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*
+del /F /Q %APPDATA%\Microsoft\Windows\Recent\CustomDestinations\*
 
 echo post cleanup
 start /wait dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
