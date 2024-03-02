8460b2e4d323839747b6480e0ded940af73d9f5b
Solomon Samuel <sissona@live.fr>
2024-03-02 18:56:52 +0100

    update

--- tmp-online/regedit.ps1
+++ tmp-online/regedit.ps1
@@ -1,11 +1,11 @@
 # Disable Telemetry
-Write-Host "[REG] [REG] Disabling Telemetry..." -ForegroundColor Cyan
+Write-Host "[REG] [REG] Disabling Telemetry" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
 
 # Disable Wi-Fi Sense
-Write-Host "[REG] Disabling Wi-Fi Sense..." -ForegroundColor Cyan
+Write-Host "[REG] Disabling Wi-Fi Sense" -ForegroundColor Cyan
 If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
 	New-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Force | Out-Null
 }
@@ -13,7 +13,7 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\Allo
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Name "Value" -Type DWord -Value 0
 
 # Disable Bing Search In Start Menu
-Write-Host "[REG] Disabling Bing Search In Start Menu..." -ForegroundColor Cyan
+Write-Host "[REG] Disabling Bing Search In Start Menu" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type DWord -Value 0
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
@@ -22,25 +22,25 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Type DWord -Value 1
 
 # Disable Start Menu Suggestions
-Write-Host "[REG] Disabling Start Menu suggestions..." -ForegroundColor Cyan
+Write-Host "[REG] Disabling Start Menu suggestions" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SystemPaneSuggestionsEnabled" -Type DWord -Value 0
 
 # Disable Automatically Installing Suggested Apps
-Write-Host "[REG] Disabling automatically installing suggested apps..." -ForegroundColor Cyan
+Write-Host "[REG] Disabling automatically installing suggested apps" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SilentInstalledAppsEnabled" -Type DWord -Value 0
 Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\SuggestedApps" -Name *
 
 # Disable Location Tracking
-Write-Host "[REG] Disabling Location Tracking..." -ForegroundColor Cyan
+Write-Host "[REG] Disabling Location Tracking" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type DWord -Value 0
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\lfsvc\Service\Configuration" -Name "Status" -Type DWord -Value 0
 
 # Disable Automatic Maps updates
-Write-Host "[REG] Disabling Automatic Maps Updates..." -ForegroundColor Cyan
+Write-Host "[REG] Disabling Automatic Maps Updates" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKLM:\SYSTEM\Maps" -Name "AutoUpdateEnabled" -Type DWord -Value 0
 
 # Disable Feedback
-Write-Host "[REG] Disabling Feedback..." -ForegroundColor Cyan
+Write-Host "[REG] Disabling Feedback" -ForegroundColor Cyan
 If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Siuf\Rules")) {
 	New-Item -Path "HKCU:\SOFTWARE\Microsoft\Siuf\Rules" -Force | Out-Null
 }
@@ -51,27 +51,27 @@ Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Siuf\Rules" -Name "NumberOfSIUF
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "DoNotShowFeedbackNotifications" -Type DWord -Value 1
 
 # Disable Advertising ID
-Write-Host "[REG] Disabling Advertising ID..." -ForegroundColor Cyan
+Write-Host "[REG] Disabling Advertising ID" -ForegroundColor Cyan
 If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo")) {
 	New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Force | Out-Null
 }
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Name "Enabled" -Type DWord -Value 0
 
 # Disable Error Reporting
-Write-Host "[REG] Disable Error Reporting..." -ForegroundColor Cyan
+Write-Host "[REG] Disable Error Reporting" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting" -Name "Disabled" -Type DWord -Value 00000001
 Stop-Service "WerSvc" -WarningAction SilentlyContinue
 Set-Service "WerSvc" -StartupType Disabled
 
 # Stop Sending Microsoft Info About How You Write
-Write-Host "Stop Sending Microsoft Info About How You Write..."
+Write-Host "[REG] Stop Sending Microsoft Info About How You Write" -ForegroundColor Cyan
 If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Input\TIPC")) {
 	New-Item -Path "HKCU:\SOFTWARE\Microsoft\Input\TIPC" -Force | Out-Null
 }
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Input\TIPC" -Name "Enabled" -Type DWord -Value 0
 
 # Disable Sharing Of Handwriting Data
-Write-Host "Disabling Sharing Of Handwriting Data..."
+Write-Host "[REG] Disabling Sharing Of Handwriting Data" -ForegroundColor Cyan
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\TabletPC")) {
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\TabletPC" | Out-Null
 }
@@ -86,7 +86,7 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\HandwritingErr
 Set-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\HandwritingErrorReports" -Name "PreventHandwritingErrorReports" -Type DWord -Value 1
 
 # Disable Windows Update Automatic Restart
-Write-Host "Disabling Windows Update Automatic Restart..."
+Write-Host "[REG] Disabling Windows Update Automatic Restart" -ForegroundColor Cyan
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU")) {
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Force | Out-Null
 }
@@ -94,15 +94,15 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUPowerManagement" -Type DWord -Value 0
 
 # Disable Fast Startup
-Write-Host "Disabling Fast Startup..."
+Write-Host "[REG] Disabling Fast Startup" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name "HiberbootEnabled" -Type DWord -Value 0
 
 # Hide Frequently Used Apps
-Write-Host "Hiding Frequently Used Apps"
+Write-Host "[REG] Hiding Frequently Used Apps" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "ShowFrequent" -Type DWord -Value 0
 
 # Disable Windows 10 Tips
-Write-Host "Disabling Windows 10 Tips..."
+Write-Host "[REG] Disabling Windows 10 Tips" -ForegroundColor Cyan
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent")) {
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" | Out-Null
 }
@@ -110,37 +110,37 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" 
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SoftLandingEnabled" -Type DWord -Value 0
 
 # Disable Look For An App In The Store
-Write-Host "Disabling Look For An App In The Store..."
+Write-Host "[REG] Disabling Look For An App In The Store" -ForegroundColor Cyan
 If (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer")) {
 	New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer" -Force | Out-Null
 }
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer" -Name "NoUseStoreOpenWith" -Type DWord -Value 1
 
 # Disable Autoplay
-Write-Host "Disabling Autoplay..."
+Write-Host "[REG] Disabling Autoplay" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Type DWord -Value 1
 
 # Disable Autorun For All Drives
-Write-Host "Disabling Autorun For All Drives..."
+Write-Host "[REG] Disabling Autorun For All Drives" -ForegroundColor Cyan
 If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {
 	New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Force | Out-Null
 }
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDriveTypeAutoRun" -Type DWord -Value 255
 
 # Hide Search Box/Button
-Write-Host "Hiding Search Box/Button..."
+Write-Host "[REG] Hiding Search Box/Button" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Type DWord -Value 0
 
 # Hide Task View Button
-Write-Host "Hiding Task View Button..."
+Write-Host "[REG] Hiding Task View Button" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Type DWord -Value 0
 
 # Change Default Explorer View To This PC
-Write-Host "Changing Default Explorer View To This PC..."
+Write-Host "[REG] Changing Default Explorer View To This PC" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "LaunchTo" -Type DWord -Value 1
 
 # Show This PC Shortcut On Desktop
-Write-Host "Showing This PC Shortcut On Desktop..."
+Write-Host "[REG] Showing This PC Shortcut On Desktop" -ForegroundColor Cyan
 If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu")) {
 	New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu" -Force | Out-Null
 }
@@ -148,11 +148,11 @@ Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Type DWord -Value 0
 
 # Faster Menu Delay
-Write-Host "Changing Menu Delay..."
+Write-Host "[REG] Changing Menu Delay" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "MenuShowDelay" -Type String -Value "50"
 
 # Stop App Suggestion
-Write-Host "Stopping App Suggestion..."
+Write-Host "[REG] Stopping App Suggestion" -ForegroundColor Cyan
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent")) {
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Force | Out-Null
 }
@@ -160,24 +160,23 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" 
 
 
 # Hide People icon
-Write-Host "Hiding People icon..."
+Write-Host "[REG] Hiding People icon" -ForegroundColor Cyan
 If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People")) {
 	New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" | Out-Null
 }
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name "PeopleBand" -Type DWord -Value 0
 
 # Disable Windows Spotlight
-Write-Host "Disabling Windows Spotlight..."
+Write-Host "[REG] Disabling Windows Spotlight" -ForegroundColor Cyan
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent")) {
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" | Out-Null
 }
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name "DisableWindowsSpotlightFeatures" -Type DWord -Value 1
 
 # Disable Xbox DVR
-Write-Host "Disablng Xbox DVR"
+Write-Host "[REG] Disablng Xbox DVR" -ForegroundColor Cyan
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR")) {
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Force | Out-Null
 }
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Name "AllowGameDVR" -Type DWord -Value 0
 Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name "GameDVR_Enabled" -Type DWord -Value 0
-
