91bdf1085d75e72cd2b79e590c2fc52dbfeab5c9
Solomon Samuel <sissona@live.fr>
2024-03-03 10:18:40 +0100

    BUG in zPOST

--- online/zpost.ps1
+++ online/zpost.ps1
@@ -1,5 +1,7 @@
 Write-Host '[Image] cleanup components' -ForegroundColor Cyan
-start /wait dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
-start /wait dism.exe /Online /Cleanup-Image /SPSuperseded
+# start /wait dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
+# start /wait dism.exe /Online /Cleanup-Image /SPSuperseded
 
+Start-Process -FilePath dism.exe -ArgumentList '/Online /Cleanup-Image /StartComponentCleanup /ResetBase' -Wait
+Start-Process -FilePath dism.exe -ArgumentList '/Online /Cleanup-Image /SPSuperseded' -Wait
 Start-Process -FilePath CleanMgr.exe -ArgumentList '/sagerun:1 /VeryLowDisk /AUTOCLEAN' -Wait
