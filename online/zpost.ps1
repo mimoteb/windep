16c52a965e53aa1424f8eeb4eb2167b140ff1acb
Solomon Samuel <sissona@live.fr>
2024-03-03 12:21:18 +0100

    add reg command

--- online/zpost.ps1
+++ online/zpost.ps1
@@ -5,3 +5,6 @@ Write-Host '[Image] cleanup components' -ForegroundColor Cyan
 Start-Process -FilePath dism.exe -ArgumentList '/Online /Cleanup-Image /StartComponentCleanup /ResetBase' -Wait
 Start-Process -FilePath dism.exe -ArgumentList '/Online /Cleanup-Image /SPSuperseded' -Wait
 Start-Process -FilePath CleanMgr.exe -ArgumentList '/sagerun:1 /VeryLowDisk /AUTOCLEAN' -Wait
+
+regedit /s machine.reg
+regedit /s user.reg
