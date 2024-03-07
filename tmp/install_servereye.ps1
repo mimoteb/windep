55601665e3b93dce7f55d064846da6a7d5a86724
Solomon Samuel <sissona@live.fr>
2024-03-07 19:10:56 +0100

    added server eye installation script to research

--- tmp/install_servereye.ps1
+++
@@ -1,6 +0,0 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-$FileName = $env:TEMP + "\servereye.exe"
-$Link = "https://occ.server-eye.de/download/se/ServerEyeSetup.exe"
-$client = new-object System.Net.WebClient
-$client.DownloadFile($Link, $FileName)
-& $FileName /install /passive /norestart
