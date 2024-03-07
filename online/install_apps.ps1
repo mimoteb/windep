7dcd12f0f1f6215a3980bee99282688a770d6ce1
Solomon Samuel <sissona@live.fr>
2024-03-07 13:06:12 +0100

    check if ms365 is installed before starting download/install

--- online/install_apps.ps1
+++ online/install_apps.ps1
@@ -119,7 +119,40 @@ if (-not (Test-Path $checkPath)) {
     Write-Host "Firefox is already installed."
 }
 
-Write-Host "Downloading MS365"
-$office_exe = Join-Path $scriptDirectory 'apps\officesetup.exe'
-$office_config = Join-Path $scriptDirectory 'lists\officede.xml'
-Start-Process -Wait -FilePath $office_exe -ArgumentList "/download", $office_config
+
+# Check if Office is installed
+$officeInstalled = $false
+
+# List of common Office executable names
+$officeExecutables = @(
+    "excel.exe",
+    "winword.exe",
+    "powerpnt.exe",
+    "outlook.exe",
+    "mspub.exe",
+    "onenote.exe",
+    "lync.exe"
+)
+
+# Check if any Office executable exists in Program Files directory
+$programFilesPath = [System.IO.Path]::Combine($env:ProgramFiles, "Microsoft Office")
+
+foreach ($executable in $officeExecutables) {
+    $executablePath = Get-ChildItem -Path $programFilesPath -Recurse -Filter $executable -File | Select-Object -ExpandProperty FullName
+    if ($executablePath) {
+        Write-Host '[Microsoft 365] The Software is already installed on the system' -foregroundcolor Yellow
+        $officeInstalled = $true
+        break
+    }else{
+        if ($officeInstalled) {
+            Write-Host "Microsoft Office is installed."
+        } else {
+            Write-Host "Microsoft Office is not installed."
+        }
+        
+        Write-Host "Downloading MS365"
+        $office_exe = Join-Path $scriptDirectory 'apps\officesetup.exe'
+        $office_config = Join-Path $scriptDirectory 'lists\officede.xml'
+        Start-Process -Wait -FilePath $office_exe -ArgumentList "/download", $office_config
+    }
+}
