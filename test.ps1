1c6f0e9ee5076e4df9fd9d16ec5d97ea0a7ba14f
Solomon Samuel <sissona@live.fr>
2024-03-07 20:10:52 +0100

    removed mistakes

--- test.ps1
+++ test.ps1
@@ -2,7 +2,7 @@ $tempDirectory = [System.IO.Path]::GetTempPath()
 
 # Combine the temporary directory path with the desired subdirectory
 $BaseDir = Join-Path $tempDirectory 'windep'
-write-host '[$BaseDir] Base Dir'
+Write-Host '[$BaseDir] Base Dir'
 # List all drives
 $drives = Get-PSDrive -PSProvider FileSystem
 
@@ -14,5 +14,5 @@ if ($null -ne $localWinDep) {
     $localWinDepLetter = $localWinDep.Name
     Write-Host "[$localWinDepLetter] Found 'windep'" -ForegroundColor Green
 } else {
-    Write-Host "[$localWinDepLetter] No drive found with 'windep' in its root path." -ForegroundColor Red
+    Write-Host "No drive found with 'windep' in its root path." -ForegroundColor Red
 }
