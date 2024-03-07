2eeb4fc6cccfe38161aff32bd330737b1de05848
Solomon Samuel <sissona@live.fr>
2024-03-07 20:18:14 +0100

    finally removed the full removal script of blatwares

--- online/debloat/debloat.ps1
+++
@@ -1,7 +0,0 @@
-write-host '[Info] removing bloatwares' -ForegroundColor Yellow
-
-
-$tempPath = [System.IO.Path]::Combine($env:TEMP, 'windep-main\config\AppxPackage.lst')
-
-Write-Host 'removing windows AppxProvisionedPackage'
-
