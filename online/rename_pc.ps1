79dde863707113420fd15ebb6d42ca97eca89d4d
Solomon Samuel <sissona@live.fr>
2024-03-07 12:37:26 +0100

    added pc name generator

---
+++ online/rename_pc.ps1
@@ -0,0 +1,5 @@
+# Generate a random integer between 0 and 10000
+$generatedNumber = Get-Random -Minimum 0 -Maximum 10001
+
+# Display the result using Write-Host
+Rename-Computer -NewName "PC$generatedNumber" -Force
