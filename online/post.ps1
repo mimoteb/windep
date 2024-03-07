36742e3388f703a37c5d9dca0dcf72370182cf68
Solomon Samuel <sissona@live.fr>
2024-03-07 20:24:29 +0100

    add more informatiive outputs

--- online/post.ps1
+++ online/post.ps1
@@ -3,9 +3,9 @@ Write-Host '[Image] cleanup components' -ForegroundColor Cyan
 # start /wait dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 # start /wait dism.exe /Online /Cleanup-Image /SPSuperseded
 # relative paths should be used here
-
+Write-Host '[Reset Base] resetting the base'
 Start-Process -FilePath dism.exe -ArgumentList '/Online /Cleanup-Image /StartComponentCleanup /ResetBase' -Wait
 Start-Process -FilePath dism.exe -ArgumentList '/Online /Cleanup-Image /SPSuperseded' -Wait
 Start-Process -FilePath CleanMgr.exe -ArgumentList '/sagerun:1 /VeryLowDisk /AUTOCLEAN' -Wait
-
+Write-Host '[Compacting OS] This process will take some time, disable the Antivirus to speed up the process' -ForegroundColor Cyan
 Start-Process -FilePath compact.exe -ArgumentList '/CompactOs:always'
