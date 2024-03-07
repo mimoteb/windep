6ac7ad1982f063d2b91405eac23108c6093d0fbf
=Solomon Samuel <=sissona@live.fr>
2024-03-07 19:31:02 +0100

    find the local Win-dep files

--- test.ps1
+++ test.ps1
@@ -2,8 +2,17 @@ $tempDirectory = [System.IO.Path]::GetTempPath()
 
 # Combine the temporary directory path with the desired subdirectory
 $BaseDir = Join-Path $tempDirectory 'windep-main'
-Write-Host $setupPath
 
-foreach ($d in Get-PSDrive) {
-    Write-Host $d
+# List all drives
+$drives = Get-PSDrive -PSProvider FileSystem
+
+# Find the first drive with "windep" in its root path
+$localWinDep = $drives | Where-Object { Test-Path (Join-Path $_.Root "\" -ChildPath "windep") } | Select-Object -First 1
+
+# Display the result
+if ($localWinDep -ne $null) {
+    $localWinDepLetter = $localWinDep.Name
+    Write-Host "Found 'windep' on drive $localWinDepLetter" -ForegroundColor Green
+} else {
+    Write-Host "No drive found with 'windep' in its root path." -ForegroundColor Red
 }
