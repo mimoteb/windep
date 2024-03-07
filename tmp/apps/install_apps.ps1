827b8b091f4567e43d7ca6dcdfea67fd644f8e3b
Solomon Samuel <sissona@live.fr>
2024-03-07 19:01:46 +0100

    checkpoint

--- tmp/apps/install_apps.ps1
+++ tmp/apps/install_apps.ps1
@@ -11,61 +11,6 @@ if (-not (Test-Path $appsDirectory)) {
     New-Item -Path $appsDirectory -ItemType Directory
     Write-Host "Directory 'apps' created."} else {}
 
-
-
-$checkPath = "C:\Program Files\7-Zip\7z.exe"
-$setupPath = Join-Path $scriptDirectory "apps\7z.msi"
-
-if (-not (Test-Path $checkPath)) {
-    if (-not (Test-Path $setupPath)) {
-        try {
-            $client = New-Object System.Net.WebClient
-            "https://7-zip.org/a/.msi"
-            $client.DownloadFile("https://7-zip.org/a/7z2301-x64.msi", $setupPath)
-            Write-Host "7-Zip setup file downloaded."
-        } catch {
-            Write-Host "An error occurred while downloading the 7-Zip setup file."
-        }
-    }
-
-    try {
-        Start-Process msiexec.exe -Wait -ArgumentList "/i `"$setupPath`" /quiet"
-        Write-Host "Installed 7-Zip"
-    } catch {
-        Write-Host "An error occurred while installing 7-Zip."
-    }
-} else {
-    Write-Host "7-Zip is already installed."
-}
-
-
-$setupPath = Join-Path $appsDirectory "ZoomOutlookPluginSetup.msi"
-
-if (Test-Path $setupPath) {
-    try {
-        Start-Process msiexec.exe -Wait -ArgumentList "/i `"$setupPath`" /quiet"
-        Write-Host 'Installed Zoom Outlook Plugin'
-    } catch {
-        Write-Host 'An error occurred while installing Zoom Outlook Plugin.'
-        # Handle if a terminating exception happens
-    }
-} else {
-    Write-Host 'Zoom Outlook Plugin setup file not found.'
-}
-
-# Adobe Acrobat reader
-$setupPath = Join-Path $scriptDirectory "Apps\acrordr2020\Setup.exe"
-if (Test-Path $setupPath) {
-    try {
-        Start-Process $setupPath -Wait -ArgumentList '/sPB /rs /rps /sl "1031"'
-        Write-Host 'Installed Adobe Acrobat Reader'
-    } catch {
-        # Handle if a terminating exception happens
-    }
-} else {
-    Write-Host 'Adobe Acrobat Reader setup file not found.'
-}
-
 #AnyDesk
 $check_path = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AnyDesk Hanebutt IT-Consult GmbH AnyDesk Client\AnyDesk Hanebutt IT-Consult GmbH AnyDesk Client.lnk"
 $setupPath = Join-Path $scriptDirectory "apps\anydesk.msi"
@@ -90,29 +35,4 @@ if (-not (Test-Path $check_path)) {
             Copy-Item -Path $check_path -Destination (Join-Path $env:USERPROFILE "Desktop\AnyDesk Hanebutt IT-Consult GmbH AnyDesk Client.lnk") -ErrorAction Ignore
         } catch{ Write-Host "Error in "$setupPath}
     }
-}
-
-# Firefox
-$checkPath = "C:\Program Files\Mozilla Firefox\firefox.exe"
-$setupPath = Join-Path $scriptDirectory "apps\firefox.exe"
-
-if (-not (Test-Path $checkPath)) {
-    if (-not (Test-Path $setupPath)) {
-        try {
-            $client = New-Object System.Net.WebClient
-            $client.DownloadFile("https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=de", $setupPath)
-            Write-Host "Firefox setup file downloaded."
-        } catch {
-            Write-Host "An error occurred while downloading the Firefox setup file."
-        }
-    }
-
-    try {
-        Start-Process .\Apps\firefox.exe -Wait -ArgumentList '-ms -ma'
-        Write-Host "Installed Firefox"
-    } catch {
-        Write-Host "An error occurred while installing Firefox."
-    }
-} else {
-    Write-Host "Firefox is already installed."
 }
