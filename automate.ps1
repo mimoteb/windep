f58670d09615a1c6002e9810dab39ffaafb0566c
Solomon Samuel <sissona@live.fr>
2024-03-07 12:49:23 +0100

    improved automate to remove the repo from local machine if existed

--- automate.ps1
+++ automate.ps1
@@ -4,6 +4,24 @@ $repo = 'https://github.com/mimoteb/windep/archive/refs/heads/main.zip'
 $repo_OutFile = "$env:TEMP\windep-main.zip"
 $extractedPath = "$env:TEMP"
 
+# Remove WinDep-Main directory in Temp if Existed 
+$tempDirectory = [System.IO.Path]::GetTempPath()
+$targetDirectory = Join-Path $tempDirectory 'windep-main'
+
+try {
+    # Check if the directory exists
+    if (Test-Path -Path $targetDirectory -PathType Container) {
+        # Directory exists, so remove it forcibly
+        Remove-Item -Path $targetDirectory -Recurse -Force
+        Write-Host "Directory $targetDirectory has been forcibly removed."
+    } else {
+        Write-Host "Directory $targetDirectory does not exist."
+    }
+} catch {
+    Write-Host "An error occurred: $_"
+}
+
+
 try {
     Write-Host "[Downloading] from $repo" -ForegroundColor Cyan
     Write-Host "[Info] to $repo_OutFile" -ForegroundColor Yellow
