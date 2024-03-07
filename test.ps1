e37e1a29a292001ce7785a2a1b357d62b6849f0f
Solomon Samuel <sissona@live.fr>
2024-03-07 19:07:28 +0100

    moved test script out

--- test.ps1
+++ test.ps1
@@ -1,6 +1,5 @@
 $tempDirectory = [System.IO.Path]::GetTempPath()
 
 # Combine the temporary directory path with the desired subdirectory
-$scriptDirectory = Join-Path $tempDirectory 'windep'
-$setupPath = Join-Path $scriptDirectory "apps\7z.msi"
+$BaseDir = Join-Path $tempDirectory 'windep-main'
 Write-Host $setupPath
