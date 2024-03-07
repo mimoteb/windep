accba1c5680f84537be35840ad108792b796cfe2
Solomon Samuel <sissona@live.fr>
2024-03-07 21:44:13 +0100

    changed variable names, working on anydesk installation from local source

--- online/Apps/acrobat_reader.ps1
+++ online/Apps/acrobat_reader.ps1
@@ -6,13 +6,13 @@
 $drives = Get-PSDrive -PSProvider FileSystem
 
 # Find the first drive with "windep" in its root path
-$localWinDep = $drives | Where-Object { Test-Path (Join-Path $_.Root "\" -ChildPath "windep") } | Select-Object -First 1
-localWinDepLetter
+$IsWinDep = $drives | Where-Object { Test-Path (Join-Path $_.Root "\" -ChildPath "windep") } | Select-Object -First 1
+
 # Display the result
-if ($null -ne $localWinDep) {
-    $localWinDepLetter = $localWinDep.Name 
-    $WinDep = (Join-Path $localWinDepLetter '\windep\')
-    Write-Host "[$localWinDepLetter] Found 'windep'" -ForegroundColor Green
+if ($null -ne $IsWinDep) {
+    $DriveLetter = $IsWinDep.Name 
+    $WinDep = (Join-Path $DriveLetter '\windep\')
+    Write-Host "[$WinDep] Found 'windep'" -ForegroundColor Green
     $setupPath = (Join-Path $WinDep "Apps\acrordr2020\Setup.exe")
     if (Test-Path $setupPath) {
         try {Start-Process $setupPath -Wait -ArgumentList '/sPB /rs /rps /sl "1031"'
--- online/Apps/anydesk.ps1
+++ online/Apps/anydesk.ps1
@@ -1,7 +1,41 @@
 # Description
 
-#AnyDesk
-$check_path = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AnyDesk Hanebutt IT-Consult GmbH AnyDesk Client\AnyDesk Hanebutt IT-Consult GmbH AnyDesk Client.lnk"
+# AnyDesk Installation from Local Source
+
+# Check if the software is already installed
+$CheckPath = [System.IO.Path]::Combine($env:ProgramData, 'Microsoft\Windows\Start Menu\Programs')
+$Filter = '*Anydesk*.lnk'
+
+# Check if a file with the specified criteria exists
+$IsSoftwareInstalled = Get-ChildItem -Path $CheckPath -Filter $Filter -File | Select-Object -First 1 -ErrorAction SilentlyContinue
+
+
+
+if ($IsSoftwareInstalled) {
+    Write-Host "[AnyDesk] Found in $CheckPath" -ForegroundColor Green
+    } else {
+
+        # Find the first drive with "windep" in its root path
+        $IsWinDep = $drives | Where-Object { Test-Path (Join-Path $_.Root "\" -ChildPath "windep") } | Select-Object -First 1
+        # Check if local windep directory exists
+        if ($null -ne $IsWinDep) {
+            $DriveLetter = $IsWinDep.Name 
+            $WinDep = (Join-Path $DriveLetter '\windep\')
+            Write-Host "[$WinDep] Found 'windep'" -ForegroundColor Green
+            $setupPath = (Join-Path $WinDep "Apps\acrordr2020\Setup.exe")
+            if (Test-Path $setupPath) {
+                try {Start-Process $setupPath -Wait -ArgumentList '/sPB /rs /rps /sl "1031"'
+                    Write-Host 'Installed Adobe Acrobat Reader'
+                } catch {}
+            } else {Write-Host '[Adobe Reader] Installation was not found in Drive\Windep\Apps\AcroRDR2020\Setup.exe'}
+        } else {Write-Host "[Drive\WinDep] Directory was not found." -ForegroundColor Red}
+        Write-Host "[AnyDesk] is being installed"
+        Write-Host "No file matching the criteria found in $CheckPath."
+}
+
+
+
+
 $setupPath = Join-Path $BaseDir "apps\anydesk.msi"
 if (-not (Test-Path $check_path)) {
     # The shortcut does not exist
