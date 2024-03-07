be5a77be5a1030204b0ee9feda41b6c7dbe006a7
Solomon Samuel <sissona@live.fr>
2024-03-07 22:48:09 +0100

    add functionality to auto detect reg files and import them

--- online/registry/registry.ps1
+++ online/registry/registry.ps1
@@ -12,6 +12,11 @@ Start-Process regedit -ArgumentList '/s', $Machine
 Start-Process regedit -ArgumentList '/s', $User
 
 # finally execute all the reg files in the same directory
-$
+$RegFiles = Get-ChildItem -Path $PSDir -Filter "*.reg" -Recurse -ErrorAction SilentlyContinue
+foreach ($File in $RegFiles) {
+    Write-Host '[REG] The following file found in, and will be executed: '$File -ForegroundColor Cyan
+    Start-Process regedit -ArgumentList '/s', $File -NoNewWindow -Wait
+}
+
 # Finally delete this script
 Remove-Item -Path $MyInvocation.MyCommand.Source -Force -ErrorAction SilentlyContinue
