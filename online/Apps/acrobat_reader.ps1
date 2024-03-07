5b58a1a745b09156c917100201e93c811a7ced0a
Solomon Samuel <sissona@live.fr>
2024-03-07 19:18:01 +0100

    changed reader

--- online/Apps/acrobat_reader.ps1
+++ online/Apps/acrobat_reader.ps1
@@ -1,13 +1,19 @@
+# Description
+# Install Adobe Acrobat Reader 2020
 
+# Check if the Setup files are already downloaded i.e existed on an external source
+foreach ($d in Get-PSDrive) {
+    Write-Host $d
+}
 # Adobe Acrobat reader
 $setupPath = Join-Path $BaseDir "Apps\acrordr2020\Setup.exe"
 if (Test-Path $setupPath) {
     try {
-        Start-Process $setupPath -Wait -ArgumentList '/sPB /rs /rps /sl "1031"'
-        Write-Host 'Installed Adobe Acrobat Reader'
+        #Start-Process $setupPath -Wait -ArgumentList '/sPB /rs /rps /sl "1031"'
+        #Write-Host 'Installed Adobe Acrobat Reader'
     } catch {
         # Handle if a terminating exception happens
     }
 } else {
-    Write-Host 'Adobe Acrobat Reader setup file not found.'
+    #Write-Host 'Adobe Acrobat Reader setup file not found.'
 }
