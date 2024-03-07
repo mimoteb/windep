f16bf78e0b73915ca45aef7dc456f7ce1e3d9def
Solomon Samuel <sissona@live.fr>
2024-03-07 20:41:15 +0100

    add more function to detect battery

--- online/tmp/battery.ps1
+++ online/tmp/battery.ps1
@@ -9,3 +9,17 @@ if ($batteryInfo) {
 } else {
     Write-Host "No battery found on this computer."
 }
+
+
+########################## Or in Powershell 7
+# Check if the computer has a battery
+$batteryInfo = Get-CimInstance -ClassName Win32_Battery -ErrorAction SilentlyContinue
+
+if ($batteryInfo) {
+    Write-Host "Battery information available:"
+    Write-Host "Manufacturer: $($batteryInfo.Manufacturer)"
+    Write-Host "Status: $($batteryInfo.Status)"
+    Write-Host "Battery Level: $($batteryInfo.EstimatedChargeRemaining)%"
+} else {
+    Write-Host "No battery found on this computer."
+}
