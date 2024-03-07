bb800481d3b7062faa6d431b415f18a7d451eebc
Solomon Samuel <sissona@live.fr>
2024-03-07 14:52:35 +0100

    adding 7z and zoom plugin to winget

---
+++ online/winget.ps1
@@ -0,0 +1,26 @@
+# Get the latest download URL
+# $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+# $URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+#         Select-Object -ExpandProperty "assets" |
+#         Where-Object { $_.browser_download_url -like '*.msixbundle' } |
+#         Select-Object -ExpandProperty "browser_download_url"
+
+# # Set the download path to %temp%
+# $downloadPath = [System.IO.Path]::Combine($env:TEMP, "winget-cli.msix")
+
+# # Download the MSIX bundle using .NET WebClient
+# $webClient = New-Object System.Net.WebClient
+# $webClient.DownloadFile($URL, $downloadPath)
+
+# # Install the MSIX bundle
+# Add-AppxPackage -Path $downloadPath
+
+# # Delete the downloaded file
+# Remove-Item $downloadPath
+
+Add-AppxPackage -Path "D:\data\winget-cli.msixbundle"
+
+winget install 7zip.7zip --force --silent
+
+winget install Zoom.ZoomOutlookPlugin --force --silent
+
