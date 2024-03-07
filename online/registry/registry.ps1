1078f4fe3f198fbe7a8ffbb3449a7c5b54e9ad55
Solomon Samuel <sissona@live.fr>
2024-03-07 22:52:18 +0100

    fully automated registry keys importation

--- online/registry/registry.ps1
+++ online/registry/registry.ps1
@@ -1,16 +1,6 @@
 # Description
 
 $PSDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$Machine = (Join-Path $PSDir "\Machine.reg")
-$User = (Join-Path $PSDir "\User.reg")
-$TempDir = [System.IO.Path]::GetTempPath()
-$BaseDir = Join-Path $TempDir 'windep-main'
-
-
-Start-Process regedit -ArgumentList '/s', $Machine
-# Check if the user is not an administrator
-Start-Process regedit -ArgumentList '/s', $User
-
 # finally execute all the reg files in the same directory
 $RegFiles = Get-ChildItem -Path $PSDir -Filter "*.reg" -Recurse -ErrorAction SilentlyContinue
 foreach ($File in $RegFiles) {
