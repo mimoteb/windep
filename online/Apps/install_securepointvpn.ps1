f895e14ea4d46bffe040cffa3d60b798a5e47d65
Solomon Samuel <sissona@live.fr>
2024-03-07 19:05:17 +0100

    added installation script for securepoint vpn

--- online/Apps/install_securepointvpn.ps1
+++ online/Apps/install_securepointvpn.ps1
@@ -1,3 +1,4 @@
+# Execute script only when laptop is detected i.e. when there is a battery
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 # this code must be runned as an administrator
 $Link = "https://sourceforge.net/projects/securepoint/files/latest/download"
