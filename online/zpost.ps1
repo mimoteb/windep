cd7b532fb80ade13003257bc912df9103c249a3c
Solomon Samuel <sissona@live.fr>
2024-03-07 12:13:35 +0100

    added compactOS

--- online/zpost.ps1
+++ online/zpost.ps1
@@ -1,10 +1,11 @@
 Write-Host '[Image] cleanup components' -ForegroundColor Cyan
 # start /wait dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 # start /wait dism.exe /Online /Cleanup-Image /SPSuperseded
+regedit /s machine.reg
+regedit /s user.reg
 
 Start-Process -FilePath dism.exe -ArgumentList '/Online /Cleanup-Image /StartComponentCleanup /ResetBase' -Wait
 Start-Process -FilePath dism.exe -ArgumentList '/Online /Cleanup-Image /SPSuperseded' -Wait
 Start-Process -FilePath CleanMgr.exe -ArgumentList '/sagerun:1 /VeryLowDisk /AUTOCLEAN' -Wait
 
-regedit /s machine.reg
-regedit /s user.reg
+Start-Process -FilePath compact.exe -ArgumentList '/CompactOs:always'
