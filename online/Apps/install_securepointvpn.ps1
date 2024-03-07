d405cb6583403dd3ccfda0b24180a8b070cdb566
Solomon Samuel <sissona@live.fr>
2024-03-07 23:15:53 +0100

    add a new task to openvpn installation script

--- online/Apps/install_securepointvpn.ps1
+++ online/Apps/install_securepointvpn.ps1
@@ -1,5 +1,6 @@
 # Check if SecurePoint OpenVPN is installed at the beginning
 # Execute script only when laptop is detected i.e. when there is a battery
+# Download using webclient
 # Secure Point language selection prompt suspends the automation
 # Description
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
