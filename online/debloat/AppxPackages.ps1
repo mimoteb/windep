004ec6410d45f1eb45e7dc9c2c88b95e939c6a5c
Solomon Samuel <sissona@live.fr>
2024-03-17 01:56:32 +0100

    updated console output messages

--- online/debloat/AppxPackages.ps1
+++ online/debloat/AppxPackages.ps1
@@ -7,15 +7,15 @@ foreach ($item in $JunkItems) {
     $item = $item.Trim()
 
     try {
+        Write-Host ('[AppxPackage] Removing {0}' -f $item) -ForegroundColor Cyan
         Get-AppxPackage -all *$item* | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
         Get-AppxPackage -all *$item* | Remove-AppPackage -ErrorAction SilentlyContinue
         # WRONG: Remove-AppxPackage -Package $item -AllUsers -ErrorAction SilentlyContinue
-        Write-Host "[$item] AppxPackag removed: $item" -ForegroundColor Green
+        
     } catch {Write-Host "[$item]: Failed to remove $item - $_" -ForegroundColor Red}
 
     try {
         Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -match [regex]::Escape($item) } | Remove-AppxProvisionedPackage -AllUsers -ErrorAction SilentlyContinue
-        Write-Host "[$item] removed" -ForegroundColor Green
     } catch {Write-Host "[$item]: Failed to remove - $_" -ForegroundColor Red}
 }
 
