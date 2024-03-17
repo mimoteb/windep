926011096b4f82d05bb89703a338f6693f306cd8
Solomon Samuel <sissona@live.fr>
2024-03-17 01:22:52 +0100

    corrected tasks disabled script

--- online/tasks/disable_tasks.ps1
+++ online/tasks/disable_tasks.ps1
@@ -1,72 +1,22 @@
 # this file has mistakes
 # get-scheduledtask | where-object {$_.taskname -like "ScheduledDefrag"} | disable-scheduledtask
-# TaskPath should look like "\dir1\subdir\"
+# TaskPath should look like "\dir1\subdir\" when using get-scheduledtask
+# when disabling or enabling the task taskname can be full path + task name
 # Description
-write-host 'Disabling Tasks'
-$tempPath = [System.IO.Path]::Combine($env:TEMP, 'windep-main\config\tasks.lst')
-$items = Get-Content -Path $tempPath
-# Read the file line by line
-Get-Content $items | ForEach-Object {
-    # Split each line into path and task name using "\" as the delimiter
-    $parts = $_.Trim() -split "\\"
+Write-Host "Disabling Tasks" -ForegroundColor Yellow
 
-    # Check if there are at least two parts (path and task name)
-    if ($parts.Length -ge 2) {
-        # Extract the path and task name
-        $taskPath = $parts[0..($parts.Length - 2)] -join "\"
-        $taskName = $parts[-1]
-        Write-Host '[Task] Disabling' $taskName -ForegroundColor Cyan
-        # Disable the task using the task path
-        Disable-ScheduledTask -TaskPath $taskPath -TaskName $taskName
+$tempFilePath = Join-Path -Path $env:TEMP -ChildPath "windesk-main/config/tasks.lst"
+
+if (Test-Path -Path $tempFilePath) {
+    $tasksToDisable = Get-Content -Path $tempFilePath
+
+    foreach ($task in $tasksToDisable) {
+        $taskName = Split-Path -Path $task -Leaf
+
+        Write-Host ("[Task] Disabling '{0}'..." -f $taskName) -ForegroundColor Cyan
+        Disable-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
     }
-    else {
-        Write-Host "[ERROR] Invalid line: $_" -ForegroundColor Red
-    }
-
 }
-
-
-$tasks = @(
-	"Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
-	"Microsoft\Windows\Application Experience\ProgramDataUpdater"
-	"Microsoft\Windows\Autochk\Proxy"
-	"Microsoft\Windows\Customer Experience Improvement Program\Consolidator"
-	"Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask"
-	"Microsoft\Windows\Customer Experience Improvement Program\UsbCeip"
-	"Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector"
-)
-foreach ($task in $tasks) {
-	schtasks /Change /TN $task /Disable | Out-Null
+else {
+    Write-Warning ('[WARNING] File not found: {0}' -f $tempFilePath)
 }
-
-
-# Disable Unneeded Scheduled Tasks
-Write-Host "Disabling Unneeded Scheduled Tasks..."
-schtasks /Change /TN Microsoft\Windows\CloudExperienceHost\CreateObjectTask /Disable | Out-Null
-schtasks /Change /TN Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector /Disable | Out-Null
-schtasks /Change /TN Microsoft\Windows\DiskFootprint\Diagnostics /Disable | Out-Null
-schtasks /Change /TN Microsoft\Windows\Feedback\Siuf\DmClient /Disable | Out-Null
-schtasks /Change /TN Microsoft\Windows\NetTrace\GatherNetworkInfo /Disable | Out-Null
-schtasks /Change /TN Microsoft\Windows\Windows Error Reporting\QueueReporting /Disable | Out-Null
-
-Write-Host "Disabling Scheduled Tasks..."
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Application Experience\ProgramDataUpdater"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Application Experience\StartupAppTask"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticResolver"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Maps\MapsToastTask"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Maps\MapsUpdateTask"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Shell\FamilySafetyMonitor"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\WDI\ResolutionHost"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Windows Media Sharing\UpdateLibrary"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Autochk\Proxy"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\CloudExperienceHost\CreateObjectTask"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Feedback\Siuf\DmClient"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Shell\FamilySafetyRefreshTask"
-Disable-ScheduledTask -TaskName "\Microsoft\Windows\Windows Error Reporting\QueueReporting"
-Disable-ScheduledTask -TaskName "\Microsoft\XblGameSave\XblGameSaveTask"
-Unregister-ScheduledTask -TaskName 'Optimize Drives' -Confirm:$false
-schtasks /Change /TN "Microsoft\Windows\Defrag\ScheduledDefrag" /Disable
