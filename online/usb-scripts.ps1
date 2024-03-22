e76998ac70e83c96db380069411e47d351b693a1
Solomon Samuel <sissona@live.fr>
2024-03-22 17:57:52 +0100

    moved script blocks

--- online/usb-scripts.ps1
+++ online/usb-scripts.ps1
@@ -1,12 +1,14 @@
+# Adobe Acrobat reader Installation
 $drives = Get-PSDrive -PSProvider FileSystem
 
 # Find the first drive with "windep" in its root path
 $IsWinDep = $drives | Where-Object { Test-Path (Join-Path $_.Root "\" -ChildPath "windep") } | Select-Object -First 1
-
-# Display the result
 if ($null -ne $IsWinDep) {
-    $DriveLetter = $IsWinDep.Name 
-    $WinDep = (Join-Path $DriveLetter '\windep\')
-    Write-Host "[$WinDep] Found 'windep'" -ForegroundColor Green
     
+    # this will be the main script
+    $Scripts = Get-ChildItem -path "$env:TEMP\windep\*.ps1" -Recurse | Sort-Object
+    foreach ($script in $Scripts) {
+        write-host '[Executing] :' $script -ForegroundColor Cyan
+        Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -File $Script" -Wait
     }
+}
